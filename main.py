14589c07b5fccd073083a86174ec2fa807a9e884
katzemzem <zeminaekaterina@gmail.com>
2024-07-17 15:04:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+#1st program
+print((9*5)**0.5)
+#2nd program
+print(9.99 > 9.98 and 1000!=1000.1)
+#3rd program
+print((1234%1211)+((5678%5000)//10))
+#4th program
+print((13.42*100)%1300 == (42.13//1))
